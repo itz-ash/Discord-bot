390c56fc05a928e13bab303d48c5b603712f9495
ItzNotAsh <chawlaashneet@gmail.com>
2023-02-24 03:14:09 +0530

    changed folders

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+client.login(process.env.TOKEN);
