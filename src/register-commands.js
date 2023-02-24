168a121945e33ba92e71434d50e782cce120234c
ItzNotAsh <chawlaashneet@gmail.com>
2023-02-24 15:58:05 +0530

    Update register-commands.js

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,9 +1,28 @@
-require('dotenv').config();
-const { REST ,Routes } = require('discord.js');
+import dotenv from 'dotenv'
+dotenv.config()
+import { REST, Routes } from 'discord.js';
 
 const commands = [
     {
         name: 'hi',
-        desc: 'hello'
+        description: 'hello'
     }
 ];
+
+const rest = new REST({version: '10' }).setToken(process.env.TOKEN);
+
+( async () => {
+    try {
+        console.log("registering slash commands");
+
+        await rest.put(
+            Routes.applicationGuildCommand(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ), 
+            { body: commands }
+        );
+    } catch (error) {
+        console.log(`there was error: ${error}`);
+    }
+})();
