5e8d1103b3df2d040968f7524e9cc32259ec0c7f
ItzNotAsh <chawlaashneet@gmail.com>
2023-02-24 18:30:26 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-import { Client, IntentsBitField } from 'discord.js';
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+});
 
-client.on('ready' , (c) => {
-    console.log(`${c.user.tag} is ready`)
-    })
+client.on('interactionCreate', (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-client.on('messageCreate', (msg) => {
-    if (msg.author.bot){
-        return;
-    }
-    console.log(msg.reply(`${msg}`));
-})
+  if (interaction.commandName === 'hey') {
+    return interaction.reply('hey!');
+  }
+
+  if (interaction.commandName === 'ping') {
+    return interaction.reply('Pong!');
+  }
+
+  if (interaction.commandName === 'add'){
+    const num1 = interaction.options.get('first-number').value;
+    const num2 = interaction.options.get('second-number').value;
+
+    interaction.reply(`the sum is ${num1 + num2}`);
+
+  }
+});
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,28 +1,51 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import { REST, Routes } from 'discord.js';
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 
 const commands = [
-    {
-        name: 'hi',
-        description: 'hello'
-    }
+  {
+    name: 'hey',
+    description: 'Replies with hey!',
+  },
+  {
+    name: 'ping',
+    description: 'Pong!',
+  },
+  {
+    name: 'add',
+    description: 'add two numbers',
+    options: [
+        {
+            name: 'first-number',
+            description: 'first number',
+            type: ApplicationCommandOptionType.Number,
+            required: true,
+        },
+        {
+            name: 'second-number',
+            description: 'second number',
+            type: ApplicationCommandOptionType.Number,
+            required: true,
+        }
+    ],
+  }
 ];
 
-const rest = new REST({version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-( async () => {
-    try {
-        console.log("registering slash commands");
+(async () => {
+  try {
+    console.log('Registering slash commands...');
 
-        await rest.put(
-            Routes.applicationGuildCommand(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
-            ), 
-            { body: commands }
-        );
-    } catch (error) {
-        console.log(`there was error: ${error}`);
-    }
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log('Slash commands were registered successfully!');
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
 })();
