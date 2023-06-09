917fd9078d9db43c05018de48ff17a88f88b5477
ItzNotAsh <chawlaashneet@gmail.com>
2023-06-09 18:31:41 +0530

    npm init

--- validation/dev-only.js
+++
@@ -1,9 +0,0 @@
-module.exports = (interaction, commandObj) => {
-  const id = "76637437960034715"
-  if (commandObj.devOnly) {
-    if (interaction.member.id !== '766374379600347156') {
-      interaction.reply(`This command can only be executed by ${id}`)
-      return true;
-    }
-  }
-}
